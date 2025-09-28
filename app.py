c58b7d4b5839ca741c4e6eb0e85566d9363f5eea
Haroon Khan <mrharoonkhan11@gmail.com>
2025-09-28 14:42:23 +0300

    Latest commit 2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ service_account_info = {
     "type": "service_account",
     "project_id": os.getenv("PROJECT_ID"),
     "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-    "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),  # handle line breaks
+    "private_key": os.getenv("PRIVATE_KEY"),  # handle line breaks
     "client_email": os.getenv("CLIENT_EMAIL"),
     "client_id": os.getenv("CLIENT_ID"),
     "auth_uri": os.getenv("AUTH_URI"),
